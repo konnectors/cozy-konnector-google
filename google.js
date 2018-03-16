8b910422fdbcf1013a3ed087bb8b015c3d1b411f
Enguerran <enguerran@ticabri.com>
2018-03-16 16:11:11 +0100

    refactor: use the module pattern with IIFE
    see https://addyosmani.com/resources/essentialjsdesignpatterns/book/\#modulepatternjavascript

--- google.js
+++ google.js
@@ -7,79 +7,85 @@ const url = require("url");
 const querystring = require("querystring");
 const opn = require("opn");
 
-function setupNewOAuth2Client() {
-  const {
-    client_id: CLIENT_ID,
-    client_secret: CLIENT_SECRET
-  } = require("./keys.json");
-  const REDIRECT_URL = "http://localhost:3000/oauth2callback";
-  const oAuth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
-  return oAuth2Client;
-}
-
-const googleHelper = {
-  oAuth2Client: this.oAuth2Client || setupNewOAuth2Client(),
-  getGoogleCode: function() {
-    return new Promise((resolve, reject) => {
-      // Open an http server to accept the oauth callback. In this simple example, the
-      // only request to our webserver is to /oauth2callback?code=<code>
-      const server = http
-        .createServer(async (req, res) => {
-          if (req.url.indexOf("/oauth2callback") > -1) {
-            // acquire the code from the querystring, and close the web server.
-            const { code } = querystring.parse(url.parse(req.url).query);
-            res.end(
-              `Authentication successful! Please return to the console. [code: ${code}]`
-            );
-            server.close();
-            resolve(code);
-          }
-
-          reject(new Error("oops", req, res));
-        })
-        .listen(3000, () => {
-          // open the browser to the authorize url to start the workflow
-          // Generate the url that will be used for the consent dialog.
-          const authorizeUrl = this.oAuth2Client.generateAuthUrl({
-            access_type: "offline",
-            scope: ["https://www.googleapis.com/auth/contacts.readonly"]
-          });
-          opn(authorizeUrl);
-        });
-    });
-  },
-  getTokens: async function() {
-    const conf = new ConfigStore(pkg.name);
-    const storedTokens = conf.get("google.tokens");
-    if (storedTokens) {
-      return storedTokens;
-    }
-    const newTokens = await googleHelper
-      .getGoogleCode()
-      .then(code => this.oAuth2Client.getToken(code))
-      .then(res => res.tokens);
-    conf.set("google.tokens", newTokens);
-    return newTokens;
-  },
-  resetConfigStore: () => {
-    const conf = new ConfigStore(pkg.name);
-    conf.delete("google.tokens");
-  },
-  getConnectionsList: function(personFields = ["names"]) {
-    const peopleAPI = google.people({
-      version: "v1",
-      auth: this.oAuth2Client
-    });
-    return new Promise((resolve, reject) => {
-      peopleAPI.people.connections.list(
-        { resourceName: "people/me", personFields: personFields },
-        (err, res) => {
-          if (err) reject(err);
-          resolve(res.data);
-        }
-      );
-    });
+const googleHelper = (() => {
+  function setupNewOAuth2Client() {
+    const {
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET
+    } = require("./keys.json");
+    const REDIRECT_URL = "http://localhost:3000/oauth2callback";
+    const oAuth2Client = new OAuth2Client(
+      CLIENT_ID,
+      CLIENT_SECRET,
+      REDIRECT_URL
+    );
+    return oAuth2Client;
   }
-};
+
+  return {
+    oAuth2Client: this.oAuth2Client || setupNewOAuth2Client(),
+    getGoogleCode: function() {
+      return new Promise((resolve, reject) => {
+        // Open an http server to accept the oauth callback. In this simple example, the
+        // only request to our webserver is to /oauth2callback?code=<code>
+        const server = http
+          .createServer(async (req, res) => {
+            if (req.url.indexOf("/oauth2callback") > -1) {
+              // acquire the code from the querystring, and close the web server.
+              const { code } = querystring.parse(url.parse(req.url).query);
+              res.end(
+                `Authentication successful! Please return to the console. [code: ${code}]`
+              );
+              server.close();
+              resolve(code);
+            }
+
+            reject(new Error("oops", req, res));
+          })
+          .listen(3000, () => {
+            // open the browser to the authorize url to start the workflow
+            // Generate the url that will be used for the consent dialog.
+            const authorizeUrl = this.oAuth2Client.generateAuthUrl({
+              access_type: "offline",
+              scope: ["https://www.googleapis.com/auth/contacts.readonly"]
+            });
+            opn(authorizeUrl);
+          });
+      });
+    },
+    getTokens: async function() {
+      const conf = new ConfigStore(pkg.name);
+      const storedTokens = conf.get("google.tokens");
+      if (storedTokens) {
+        return storedTokens;
+      }
+      const newTokens = await googleHelper
+        .getGoogleCode()
+        .then(code => this.oAuth2Client.getToken(code))
+        .then(res => res.tokens);
+      conf.set("google.tokens", newTokens);
+      return newTokens;
+    },
+    resetConfigStore: () => {
+      const conf = new ConfigStore(pkg.name);
+      conf.delete("google.tokens");
+    },
+    getConnectionsList: function(personFields = ["names"]) {
+      const peopleAPI = google.people({
+        version: "v1",
+        auth: this.oAuth2Client
+      });
+      return new Promise((resolve, reject) => {
+        peopleAPI.people.connections.list(
+          { resourceName: "people/me", personFields: personFields },
+          (err, res) => {
+            if (err) reject(err);
+            resolve(res.data);
+          }
+        );
+      });
+    }
+  };
+})();
 
 module.exports = googleHelper;
