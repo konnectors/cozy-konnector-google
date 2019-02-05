83238a44144d19e5afe6167e4093fd4b3b4d37df
Cédric Messiant <cedric.messiant@gmail.com>
2019-02-05 16:52:46 +0100

    refactor: Add getCozyMetadata function

--- src/getCozyToGoogleStrategy.js
+++ src/getCozyToGoogleStrategy.js
@@ -1,7 +1,6 @@
 const get = require('lodash/get')
-const union = require('lodash/union')
 
-const { ADD_COZY_METADATA, APP_NAME } = require('./constants')
+const getCozyMetadata = require('./getCozyMetadata')
 const transpiler = require('./transpiler')
 
 const getCozyToGoogleStrategy = (cozyClient, googleUtils, sourceAccountId) => ({
@@ -26,32 +25,9 @@ const getCozyToGoogleStrategy = (cozyClient, googleUtils, sourceAccountId) => ({
   save: cozyContact =>
     googleUtils.createContact(transpiler.toGoogle(cozyContact)),
   afterSave: (cozyContact, googleContact) => {
-    let additionalData = {}
-    if (ADD_COZY_METADATA) {
-      const updatedByApps = union(cozyContact.cozyMetadata.updatedByApps, [
-        APP_NAME
-      ])
-      const now = new Date().toISOString()
-      additionalData = {
-        cozyMetadata: {
-          updatedAt: now,
-          updatedByApps,
-          sync: {
-            [sourceAccountId]: {
-              konnector: APP_NAME,
-              lastSync: now,
-              remoteRev: googleContact.etag,
-              id: googleContact.resourceName,
-              contactsAccountsId: sourceAccountId
-            }
-          }
-        }
-      }
-    }
-
     return cozyClient.save({
       ...cozyContact,
-      ...additionalData
+      ...getCozyMetadata(cozyContact, googleContact, sourceAccountId)
     })
   }
 })
