130385d623ebd4dbfaadb2afa255066af1676a88
Yannick Lohse <contact@yannick-lohse.fr>
2019-02-12 18:31:03 +0100

    feat: Added io.cozy.contacts metadata

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,16 @@
+const packageInfos = require('../package.json')
 const ADD_COZY_METADATA = true
-const APP_NAME = 'konnector-google'
+const APP_NAME = packageInfos.name
+const APP_VERSION = packageInfos.version
 const DOCTYPE_CONTACTS = 'io.cozy.contacts'
 const DOCTYPE_CONTACTS_ACCOUNT = 'io.cozy.contacts.accounts'
+const DOCTYPE_CONTACTS_VERSION = 2
 
 module.exports = {
   ADD_COZY_METADATA,
   APP_NAME,
+  APP_VERSION,
   DOCTYPE_CONTACTS,
-  DOCTYPE_CONTACTS_ACCOUNT
+  DOCTYPE_CONTACTS_ACCOUNT,
+  DOCTYPE_CONTACTS_VERSION
 }
