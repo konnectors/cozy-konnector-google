fb892c12c0b94a6f059a3ca752e3f52b930e5a2e
Yannick Lohse <contact@yannick-lohse.fr>
2019-02-21 16:48:07 +0100

    tests: Match snapshot names

--- src/transpiler.spec.js
+++ src/transpiler.spec.js
@@ -1,14 +1,14 @@
 const transpile = require('./transpiler')
 
 describe('Transpile from google-contacts to io.cozy-contacts', () => {
-  it('Simple test', () => {
+  it('should transpile alan turing', () => {
     const source = require('../sample/alanturing.json')
 
     const transpiled = transpile.toCozy(source)
     expect(transpiled).toMatchSnapshot()
   })
 
-  it('complete tests with `sample/*.json', () => {
+  it('should transpile all contacts from `sample/*.json', () => {
     const source = require('../sample/google-contacts.json')
 
     const transpiled = source.map(row => ({
